a8e67508c29c4405a98c97c64430fcc90e322e31
Kevin Jiang <kcj1017@gmail.com>
2021-10-22 23:55:25 -0400

    Added vanilla video fragment list

--- src/index.js
+++ src/index.js
@@ -6,13 +6,17 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
   app.quit();
 }
 
+
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false,
+    }
   });
-
   // and load the index.html of the app.
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
